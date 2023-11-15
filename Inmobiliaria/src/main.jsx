cdda0798e086cdbe5690ec3cfd3f2f04df9194ff
Mailys <77859157+mailys50@users.noreply.github.com>
2023-11-15 12:00:42 -0300

    instalando  react-icons --save

--- Inmobiliaria/src/main.jsx
+++ Inmobiliaria/src/main.jsx
@@ -4,8 +4,8 @@ import Twitters from './Component/Twitter/Twitters.jsx'
 import CarouselFunction from './Component/CarouselFunction.jsx'
 import './index.css'
 // import Card from './Component/Card.jsx'
-import "bootstrap/dist/css/bootstrap.min.css";
-// import "react-bootstrap-carousel/dist/react-boostrap-carousel.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+// import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 import "animate.css/animate.min.css";
 import App from "./App.jsx"
 import Slides from "../src/Component/Slides/Slides.jsx"
